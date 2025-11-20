90903aa7e76c26e525ff0824c3b0805848b44b59
grevaurely <aurelynecia@gmail.com>
2025-11-20 09:43:44 +0700

    feat: add PlayerHealthController & DamagePlayer

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance; //ini nmnya proses singleton biar script playerhealthcontroller gmpng diakses script lain
+    public int currentHealth, maxHealth;
+  
+    private void Awake()
+    {
+        instance = this;
+    } 
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DamagePlayer()
+    {
+        currentHealth += -1;
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            gameObject.SetActive(false); //roga mati
+        }
+    }
+}
