8db022bf94bb890be725c6b839f6b895fe8e388b
there514 <theresiaestelina@gmail.com>
2025-11-23 21:00:47 +0700

    Win

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -8,12 +8,66 @@ public class ProgressBar : MonoBehaviour
     float time;
     public GameObject winScene;
     public GameObject darkMode;
+    
+    public GameObject playerObject; 
+    public float finalMoveSpeed = 8f; 
+    
+    private Rigidbody2D playerRb;
+    private Collider2D playerCol; 
+    private bool rbDisabled = false;
+    
+    private float topBoundaryY;
+    private bool timeIsUp = false; 
+    
+    private void DisablePhysicsForMovement()
+    {
+        if (playerRb != null && !rbDisabled)
+        {
+            playerRb.linearVelocity = Vector2.zero;
+            playerRb.bodyType = RigidbodyType2D.Kinematic; 
+            if (playerCol != null) 
+            {
+                playerCol.enabled = false;
+            }
+
+            rbDisabled = true;
+            Debug.Log("Fisika Player dinonaktifkan.");
+        }
+    }
+
+    private void EnablePhysicsAfterMovement()
+    {
+        if (playerRb != null && rbDisabled)
+        {
+            playerRb.bodyType = RigidbodyType2D.Dynamic; 
+            
+            if (playerCol != null) 
+            {
+                playerCol.enabled = true;
+            }
+
+            rbDisabled = false;
+            Debug.Log("Fisika Player diaktifkan kembali.");
+        }
+    }
+
     void Start()
     {
         winScene.SetActive(false);
         bar = GetComponent<Image> ();
         time = 0;
         
+        if (playerObject != null)
+        {
+            playerRb = playerObject.GetComponent<Rigidbody2D>(); 
+            playerCol = playerObject.GetComponent<Collider2D>();
+            
+            Camera mainCamera = Camera.main;
+            Vector3 topEdgeWorld = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0));
+            topBoundaryY = topEdgeWorld.y;
+
+            SpriteRenderer sr = playerObject.GetComponent<SpriteRenderer>();
+        }
     }
 
     void Update()
@@ -22,6 +76,7 @@ public class ProgressBar : MonoBehaviour
         {
             time += Time.deltaTime;
             bar.fillAmount = time / maxTime;
+            
             if(time >= 180 && time <= 240)
             {
                 darkMode.SetActive(true);
@@ -33,8 +88,42 @@ public class ProgressBar : MonoBehaviour
         }
         else
         {
-            winScene.SetActive(true);
+            if (timeIsUp == false)
+            {
+                timeIsUp = true;
+                DisablePhysicsForMovement();
+            }
+            
+            if (timeIsUp)
+            {
+                MovePlayerToTopEdge();
+            }
+
+            if (playerObject != null && playerObject.transform.position.y >= topBoundaryY)
+            {
+                winScene.SetActive(true);
+                EnablePhysicsAfterMovement();
+            }
         }
+    }
+    
+    private void MovePlayerToTopEdge()
+    {
+        if (playerObject == null) return; 
+
+        float targetY = topBoundaryY;
         
+        if (playerObject.transform.position.y < targetY)
+        {
+            playerObject.transform.Translate(Vector3.up * finalMoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 finalPos = playerObject.transform.position;
+            finalPos.y = targetY;
+            playerObject.transform.position = finalPos;
+            
+            timeIsUp = false; 
+        }
     }
 }
