9e1d83f0550b1407274f545b3ab8ea603bc8ca1b
aoo <arlas.81194@gmail.com>
2025-11-27 18:47:38 +0700

    fix: hole besar

--- Assets/Scripts/SpeedChanger.cs
+++ Assets/Scripts/SpeedChanger.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class SpeedChanger : MonoBehaviour
 {
     private float newSpeed;
-    private float originalSpeed = 5;
+    private float originalSpeed;
     public float slowPercentage = 50;
     public bool canFreeze = false;
-    public float cantMoveTime = 5f;
+    public float cantMoveTime = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter2D(Collider2D collision)
